07d3b55599c9c721996a205cb1632b4dc376a102
Verónica García <veronicagaiglesias@gmail.com>
2025-10-21 19:02:37 +0200

    feat: crear Footer y Footer.css

---
+++ src/ui/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../css/Footer.css";
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>&copy; 2025 AsturDulce. Todos los derechos reservados.</p>
+    </footer>
+  );
+};
+
+export default Footer;
