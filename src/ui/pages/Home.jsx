d7d7aa921a7002a3bbf26c685b79cf091f2753a3
Verónica García <veronicagaiglesias@gmail.com>
2025-10-21 19:04:26 +0200

    feat: actualizar Home.jsx con Header y Footer

--- src/ui/pages/Home.jsx
+++ src/ui/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "../components/Header";
+import Footer from "../components/Footer";
 import "../css/styles.css";
 
 const Home = () => {
@@ -8,6 +9,7 @@ const Home = () => {
       <Header/>
       <h1>Bienvenida a AsturDulce</h1>
       <p>Proyecto inicial con React y Vite.</p>
+      <Footer/>
     </div>
   );
 };
