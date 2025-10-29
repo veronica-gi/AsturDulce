0b27ffc56ad9d74d045dedd130bfff58d32838a4
Verónica García <veronicagaiglesias@gmail.com>
2025-10-29 16:49:19 +0100

    Actualizar DessertCard: estilo 'Leer más' en cursiva y negrita, truncado de descripción mejorado

--- src/ui/components/DessertCard.jsx
+++ src/ui/components/DessertCard.jsx
@@ -17,13 +17,15 @@ const DessertCard = ({ dessert }) => {
       <img src={images[dessert.key]} alt={dessert.name} />
       <h3>{dessert.name}</h3>
        <p className="dessert-description">{shortDescription}</p>
+       <Link to={`/recipe/${dessert.id}`} className="read-more">
+        ... Leer más
+      </Link>
+
       <p>Categoría: {dessert.category}</p>
       <p>Dificultad: {dessert.difficulty}</p>
       <div className="card-buttons">
       <button>❤️ Favorito</button>
-      <Link to={`/recipe/${dessert.id}`}>
-        <button>Leer más</button>
-      </Link>
+      
     </div>
     </div>
   );
