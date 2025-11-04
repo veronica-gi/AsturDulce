04be2e611d14af6b2cc066bf84a2490c9577ec04
Verónica García <veronicagaiglesias@gmail.com>
2025-11-04 14:04:09 +0100

    Configura base para GitHub Pages y ajusta BrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from './ui/pages/Home.jsx';
 import RecipePage from './ui/pages/RecipePage.jsx';
 import FavoritesPage from './ui/pages/FavoritesPage.jsx';
@@ -8,13 +8,13 @@ import './ui/css/styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/recipe/:id" element={<RecipePage />} />
         <Route path="/favorites" element={<FavoritesPage />} /> 
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 
